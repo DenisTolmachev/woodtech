70b38330f35aceea285d36e0810c1617b87dfc95
Denis <denis.tolmachev@gmail.com>
2023-02-22 20:57:49 +0200

    added header & footer components

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import { Container } from './Container.style';
+import { Header } from '../Header/Header';
+import { Footer } from '../Footer/Footer';
 
 export const Layout = () => {
   return (
     <Container>
+      <Header />
       <div>Layout</div>
+      <Footer />
     </Container>
   );
 };
